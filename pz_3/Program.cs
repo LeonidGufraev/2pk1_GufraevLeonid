03b7f17eea7392fadc856c7d88aeb567d7f3986d
gufraevleonid@gmail.com <gufraevleonid@gmail.com>
2022-10-26 14:39:03 +0500

    test edit

--- pz_3/Program.cs
+++ pz_3/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine();
+﻿//Вариант 4
+
+int a = Convert.ToInt32(Console.ReadLine());
+float b = Convert.ToSingle(Console.ReadLine());
+double m, n, k;
+if (b > 10.5)
+    n = (38 * a )/ (13 * (a - b));
+else
+    n = 12 * Math.Cos(0.1*b);
+if (n <= 2)
+    m = Math.Sin(a * Math.Pow(n, 2));
+else
+    m = 21*a*n+2.5* Math.Pow(n, 2);
+k = Math.Pow((a + n), 4) + Math.Pow((b + m), 2);
+Console.Write($"a = {a}, b = {b}, m = {m}, n = {n}, k = {k}");

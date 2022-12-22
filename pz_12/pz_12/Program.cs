a4acdc979570a9b2d2473f256857740d26d44a64
gufraevleonid@gmail.com <gufraevleonid@gmail.com>
2022-12-22 22:28:59 +0500

    pz_12

---
+++ pz_12/pz_12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace pz_12;
+class Program
+{
+    /*Реализуйте метод GetMax поиска наибольшего числа из произвольного количества чисел.
+            Использовать в качестве параметра массив целых чисел и выходной параметр maxValue*/
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 564, -453, 536456, 546, 1111111 };
+        int maxValue;
+        GetMax(array, out maxValue);
+        Console.WriteLine(maxValue);
+    }
+    static int GetMax(int[] a, out int maxValue)
+    {
+        maxValue = a.Max();
+        return maxValue;
+    }
+}
